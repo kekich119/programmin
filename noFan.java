49858b429d05da89d53443814524a2451f39495c
kekich119 <patowach3o@gmail.com>
2023-08-09 18:06:43 +0300

    first

---
+++ noFan.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class noFan {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введи, что ты хочешь конвертировать в рубли: ");
+        String con = in.nextLine();
+
+        if (con.equals("dollar")) {
+
+            doll();
+        }
+        if (con.equals("euro")) {
+
+        }
+
+    }
+
+    public static void doll() {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введи, сколько долларов: ");
+        int dollar = in.nextInt();
+
+        System.out.println(dollar * 93);
+        return;
+    }
+
+    public static void euri() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введи, сколько евро");
+        int euro = in.nextInt();
+
+        System.out.print(euro * 105);
+
+    }
+
+}
